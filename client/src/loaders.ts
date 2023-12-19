4faf775483d56d6c8ae1cf10a98cec90e75690ea
Rami Pellumbi <98100379+ramipellumbi@users.noreply.github.com>
2023-12-19 11:28:09 -0900

    Remove loadCSV

--- client/src/loaders.ts
+++ client/src/loaders.ts
@@ -1,19 +1,6 @@
-import Papa from 'papaparse';
-
 export const loadJSON = async (fileName: string): Promise<unknown> => {
   const response = await fetch(fileName);
   const data = await response.json();
 
   return data;
-};
-
-export const loadCsv = async (fileName: string): Promise<Papa.ParseResult<unknown>> => {
-  const response = await fetch(fileName);
-  const parsed = await response.text();
-
-  return Papa.parse(parsed, {
-    header: true,
-    dynamicTyping: true,
-    skipEmptyLines: true,
-  });
 };
