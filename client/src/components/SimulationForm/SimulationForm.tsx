2b6500b25b53340b54cf107274b1e17566f711fb
Rami Pellumbi <98100379+ramipellumbi@users.noreply.github.com>
2023-12-19 17:12:31 -0900

    Remove unused packages

--- client/src/components/SimulationForm/SimulationForm.tsx
+++ client/src/components/SimulationForm/SimulationForm.tsx
@@ -2,11 +2,9 @@
 
 import { Dispatch, SetStateAction, useState } from 'react';
 
-import { Box, List, MantineTheme, Paper, Slider, Text, useMantineTheme } from '@mantine/core';
-import { IconGripVertical } from '@tabler/icons-react';
-import { arrayMove, SortableContainer, SortableElement } from 'react-sortable-hoc';
+import { Box, Paper, Slider, Text } from '@mantine/core';
 
-import { APPARATUSES, Gender } from '@/constants';
+import { Gender } from '@/constants';
 
 type SimulationFormProps = {
   initialValue: number;
@@ -14,18 +12,8 @@ type SimulationFormProps = {
   gender: Gender;
 };
 
-export function SimulationForm({ initialValue, setSlider, gender }: SimulationFormProps) {
-  const theme = useMantineTheme();
+export function SimulationForm({ initialValue, setSlider }: SimulationFormProps) {
   const [simulationCount, setSimulationCount] = useState(initialValue);
-  const [items, setItems] = useState<string[]>([
-    'Team Event',
-    'Individual All Around',
-    ...(APPARATUSES[gender] as unknown as string[]),
-  ]);
-
-  const onSortEnd = ({ oldIndex, newIndex }: { oldIndex: number; newIndex: number }) => {
-    setItems(arrayMove(items, oldIndex, newIndex));
-  };
 
   return (
     <Paper shadow="sm" radius="md" p="lg" style={{ maxWidth: 500, margin: '10px auto' }}>
@@ -48,59 +36,6 @@ export function SimulationForm({ initialValue, setSlider, gender }: SimulationFo
           50 simulations takes about 1 minute.
         </Text>
       </Box>
-
-      <Box mb="md">
-        <Text mt={20} size="md" fw={500}>
-          Sort the order of medal importance:
-        </Text>
-        <Text mt={5} mb={5} size="sm" c={'gray'}>
-          Higher list items are weighted more heavily in the simulation.
-        </Text>
-        <SortableList items={items} onSortEnd={onSortEnd} theme={theme} />
-      </Box>
     </Paper>
   );
 }
-
-// Sortable List Item
-type SortableItemProps = {
-  value: string;
-  theme: MantineTheme;
-};
-const SortableItem = SortableElement<SortableItemProps>(({ value, theme }: SortableItemProps) => (
-  <List.Item
-    style={{
-      padding: '12px 20px',
-      cursor: 'pointer',
-      backgroundColor: theme.white,
-      border: `1px solid ${theme.colors.gray[2]}`,
-      borderRadius: theme.radius.sm,
-      boxShadow: '0 1px 3px rgba(0,0,0,0.1)',
-      display: 'flex',
-      alignItems: 'center',
-      marginBottom: '8px',
-      '&:hover': {
-        backgroundColor: theme.colors.gray[0],
-        boxShadow: '0 2px 6px rgba(0,0,0,0.15)',
-      },
-    }}
-  >
-    <IconGripVertical size={18} style={{ marginRight: '10px' }} />
-    {value}
-  </List.Item>
-));
-
-// Sortable List Container
-type SortableListProps = {
-  items: string[];
-  theme: MantineTheme;
-};
-const SortableList = SortableContainer<SortableListProps>(({ items, theme }: SortableListProps) => {
-  return (
-    <List withPadding spacing="xs">
-      {items.map((value, index) => (
-        <SortableItem key={`item-${index}`} index={index} value={value} theme={theme} />
-      ))}
-    </List>
-  );
-});
